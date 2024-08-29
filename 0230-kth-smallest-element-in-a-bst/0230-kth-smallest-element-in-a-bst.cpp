7e0c11e98ef7f45f895f79abb05204802c5d316a
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-08-29 12:13:04 +0530

    Time: 17 ms (19.8%), Space: 23.2 MB (9.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -19,35 +19,11 @@ public:
         inorder(root->right,ans);
     }
 }
-int partition(vector<int>& ans,int low,int high){
-    int i = low-1;
-    for(int j =low;j<=high-1;j++){
-        if(ans[j]<=ans[high]){
-            i++;
-            swap(ans[j],ans[i]);
-        }
-    }
-    i++;
-    swap(ans[high],ans[i]);
-    return i;
-}
-int quickSelect(vector<int>& ans,int low,int high,int k){
-    
-        int pi = partition(ans,low,high);
-        if(pi==k-1){
-            return ans[pi];
-        }else if(pi<k-1){
-            return quickSelect(ans,pi+1,high,k);
-        }
-        else{
-            return quickSelect(ans,low,pi-1,k);
-        }
-    
-}
+
 int kthSmallest(struct TreeNode* root, int k) {
     vector<int> ans;
     inorder(root,ans);
-    return quickSelect(ans,0,ans.size()-1,k);
+    return ans[k-1];
 }
 
 };
