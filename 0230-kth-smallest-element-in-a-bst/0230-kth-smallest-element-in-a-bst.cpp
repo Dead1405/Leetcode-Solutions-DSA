18f02ab2fb6322fd8ce88c82fd06265a4bf39543
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-08-29 14:36:37 +0530

    Time: 16 ms (31.96%), Space: 23 MB (33.06%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,24 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& ans,int k){
-    if(!root || ans.size() == k) return;
-    else{
-        inorder(root->left,ans,k);
-        ans.push_back(root->val);
-        inorder(root->right,ans,k);
+    int kthSmallest(struct TreeNode* root, int k) {
+        stack<struct TreeNode*> stck;
+        while(true){
+            while(root){
+                stck.push(root);
+                root = root->left;
+            }
+            root = stck.top();
+            stck.pop();
+            k--;
+            if(k==0) return root->val;
+            root = root->right;
+        }
     }
-}
-
-int kthSmallest(struct TreeNode* root, int k) {
-    vector<int> ans;
-    inorder(root,ans,k);
-    return ans[k-1];
-}
-
 };
