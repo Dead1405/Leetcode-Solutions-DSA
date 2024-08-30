65d28bbc7cd333f73b974d9ebe80661607f2af7d
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-08-30 11:37:42 +0530

    Time: 4 ms (59.28%), Space: 16.5 MB (62.25%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,35 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* mkTree(vector<int>& preorder,int start,int end){
-        if(start>end) return NULL;
-        // if(start==end){
-        //     return TreeNode(preorder[start]);
-        else{
-            int leftStart = start+1;
-            int leftEnd=start;
-            while(leftEnd+1<=end && preorder[start]>preorder[leftEnd+1]){
-                leftEnd++;
-            }
-            int rightStart = leftEnd+1;
-            int rightEnd = end;
-            TreeNode* root = new TreeNode(preorder[start]);
-            root->left = mkTree(preorder,leftStart,leftEnd);
-            root->right = mkTree(preorder,rightStart,rightEnd);
-            return root;
+    TreeNode* buildBST(vector<int>& preorder, int& index, int bound) {
+        if (index == preorder.size() || preorder[index] > bound) {
+            return NULL;
         }
+        
+        int val = preorder[index++];
+        TreeNode* root = new TreeNode(val);
+        root->left = buildBST(preorder, index, val);
+        root->right = buildBST(preorder, index, bound);
+        
+        return root;
     }
+    
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-       return mkTree(preorder,0,preorder.size()-1) ;
+        int index = 0;
+        return buildBST(preorder, index, INT_MAX);
     }
 };
