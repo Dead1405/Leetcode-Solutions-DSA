9fcd7516703be152f1e21933078cc84eafac5756
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-04 16:06:46 +0530

    Time: 0 ms (100%), Space: 44.6 MB (77.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,26 +1,15 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        //use sorted indices 
-        // Integer[] sid = new Integer[nums.length];
-        // for(int i = 0; i < nums.length; i++){
-        //     sid[i] = i;
-        // }
-        // Arrays.sort(sid, (a, b) -> Integer.compare(nums[a], nums[b]));
-        // int front = 0, rear = nums.length - 1;
-        // while(true){
-        //     if(nums[sid[front]] + nums[sid[rear]] == target){
-        //         return new int[]{sid[front], sid[rear]}; 
-        //     }
-        //     else if (nums[sid[front]] + nums[sid[rear]] > target) rear--;
-        //     else front ++;
-        // }
+    
 
-        //use hashmap
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        for(int i = 0; i < nums.length; i ++){
-            if(mp.containsKey(target - nums[i])) return new int[]{mp.get(target - nums[i]), i};
-            else mp.put(nums[i], i);
+    public int[] twoSum(int[] nums, int target) {
+
+
+        for (int i = 1 ; i < nums.length; i++) {
+            for (int y = i; y< nums.length; y++){
+                if (target == nums[y] + nums[y-i])
+                return new int[]{y , y-i};
+            }
         }
-        return new int[]{0, 0};
+        return null;
     }
 }
