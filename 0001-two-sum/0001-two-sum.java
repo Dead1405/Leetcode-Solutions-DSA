8dd96059af32c01d6feaba938f0e3eb446849c85
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-04 16:05:46 +0530

    Time: 2 ms (98.71%), Space: 45 MB (18.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         //use sorted indices 
-        Integer[] sid = new Integer[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            sid[i] = i;
+        // Integer[] sid = new Integer[nums.length];
+        // for(int i = 0; i < nums.length; i++){
+        //     sid[i] = i;
+        // }
+        // Arrays.sort(sid, (a, b) -> Integer.compare(nums[a], nums[b]));
+        // int front = 0, rear = nums.length - 1;
+        // while(true){
+        //     if(nums[sid[front]] + nums[sid[rear]] == target){
+        //         return new int[]{sid[front], sid[rear]}; 
+        //     }
+        //     else if (nums[sid[front]] + nums[sid[rear]] > target) rear--;
+        //     else front ++;
+        // }
+
+        //use hashmap
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i = 0; i < nums.length; i ++){
+            if(mp.containsKey(target - nums[i])) return new int[]{mp.get(target - nums[i]), i};
+            else mp.put(nums[i], i);
         }
-        Arrays.sort(sid, (a, b) -> Integer.compare(nums[a], nums[b]));
-        int front = 0, rear = nums.length - 1;
-        while(true){
-            if(nums[sid[front]] + nums[sid[rear]] == target){
-                return new int[]{sid[front], sid[rear]}; 
-            }
-            else if (nums[sid[front]] + nums[sid[rear]] > target) rear--;
-            else front ++;
-        }
+        return new int[]{0, 0};
     }
 }
