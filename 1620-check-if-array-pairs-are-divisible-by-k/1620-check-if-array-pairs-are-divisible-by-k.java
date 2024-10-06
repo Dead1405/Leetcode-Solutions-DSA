2c8081d574bd97855dbaedb5cc40d779848196c4
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-07 01:00:29 +0530

    Time: 6 ms (31.55%), Space: 56.7 MB (52.72%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
@@ -1,27 +1,47 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        // store all reminders in hashmap if counterpart is found remove count else
-        // increase own count keep a track of count too
+        // // store all reminders in hashmap if counterpart is found remove count else
+        // // increase own count keep a track of count too
+        // int count = 0;
+        // HashMap<Integer, Integer> mp = new HashMap<>();
+        // for (int i : arr) {
+        //     //continue for divisible elements
+        //     if (i % k == 0)
+        //         continue;
+        //     //
+        //     int rem = (k + (i % k)) % k;
+        //     int counter = k - rem;
+        //     if (mp.containsKey(counter)) {
+        //         count--;
+        //         if (mp.get(counter) == 1) mp.remove(counter);
+        //         else mp.put(counter, mp.get(counter) - 1);
+        //     } 
+        //     else {
+        //         count++;
+        //         mp.put(rem, mp.getOrDefault(rem, 0) + 1);
+        //     }
+        // }
+        // return count == 0;
+
+        //lets try array
+        int[] freq = new int[k];
         int count = 0;
-        HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i : arr) {
             //continue for divisible elements
             if (i % k == 0)
                 continue;
-            //
             int rem = (k + (i % k)) % k;
             int counter = k - rem;
-            if (mp.containsKey(counter)) {
+            if (freq[counter] > 0) {
                 count--;
-                if (mp.get(counter) == 1) mp.remove(counter);
-                else mp.put(counter, mp.get(counter) - 1);
+                freq[counter]--;
             } 
             else {
                 count++;
-                mp.put(rem, mp.getOrDefault(rem, 0) + 1);
+                freq[rem]++;
             }
-            System.out.println("" + count +" "+ rem +" "+ counter);
         }
         return count == 0;
+        
     }
 }
