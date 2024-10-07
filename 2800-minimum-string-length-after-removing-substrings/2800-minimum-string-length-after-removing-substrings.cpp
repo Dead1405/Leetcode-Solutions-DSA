2c0608871b456b0800d398de0eeb3d0b459f465e
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-07 16:23:39 +0530

    Time: 5 ms (59.38%), Space: 10 MB (97.42%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        //same keep 2 pointers 
+        int top = -1;
+        for(int i = 0; i < s.size(); i++){
+            if(top == -1){//empty stack
+                s[++top] = s[i]; 
+            }
+            else if (s[top] == 'A' && s[i] == 'B' || s[top] == 'C' && s[i] == 'D'){
+                top--;
+            }
+            else{
+                s[++top] = s[i];
+            }
+        }
+        return top + 1;
+    }
+};
