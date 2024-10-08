2670fbd233903e3ff6ea685d7a3529964670e9a9
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-08 21:05:13 +0530

    Time: 13 ms (93.08%), Space: 55.5 MB (35.23%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,48 +1,21 @@
 class Solution {
     public int minSwaps(String s) {
-        //at any number of time amount of open brackets must not exceed amount of closed ones if we tread from forward
-        //same goes for opening brackets if we tread from backwards
-        //if i am treading and i encounter a faulty parenthesis i will swap it with one from after but i will need to keep a track of where i have stored it
-        //in order to minimize number of swaps i should replace the ones that definitely need to be replaced
-        //i will be using 2 pointers
-        int swaps = 0;
-        int front = 0;
-        int rear = s.length() - 1;
-        StringBuilder s1 = new StringBuilder(s);
-        int openCount1 = 0, openCount2 = 0, closeCount1 = 0, closeCount2  = 0;
-        while(front < rear){
-            //detect first wrong
-            while(front < rear){
-                if(s1.charAt(front) == '['){
-                    openCount1++;
+        int openCount = 0, swaps = 0;
+
+        // Traverse the string
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                openCount++;  // Increment open bracket count
+            } else {
+                if (openCount > 0) {
+                    openCount--;  // Close a matched pair
+                } else {
+                    swaps++;      // Unmatched closing bracket, requires a swap
+                    openCount++;  // Account for the incoming open bracket after swap
                 }
-                else{
-                    closeCount1++;
-                }
-                if(openCount1 < closeCount1) break;
-                else front++;
             }
-            //find faulty from behind
-            while(front < rear){
-                if(s1.charAt(rear) == '['){
-                    openCount2++;
-                }
-                else{
-                    closeCount2++;
-                }
-                if(openCount2 > closeCount2) break;
-                else rear--;
-            }
-            if(front < rear){
-                char c = s1.charAt(front);
-                s1.setCharAt(front, s1.charAt(rear));
-                s1.setCharAt(rear, c);
-                swaps++;
-                closeCount1--;
-                openCount2--;
-            }
-            else return swaps;
         }
-        return 0;
+
+        return swaps;
     }
 }
