fe6fae6f41a032965ff592758d12156a195fcc52
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-12 10:45:41 +0530

    Time: 33 ms (91.3%), Space: 76.1 MB (61.14%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -1,16 +1,38 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Arrays.sort(intervals, (a, b) -> { return a[0] - b[0]; });
-        for(int[] i : intervals){
-            if(pq.isEmpty() || pq.peek() < i[0]){
-                pq.poll();
-            }
-            pq.add(i[1]);
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        for(int i = 0; i < intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
-        return pq.size();
-
-
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int max = 0;
+        int groups = 0;
+        int i = 0, j = 0;
+        while(i < intervals.length){
+            if(start[i] <= end[j]){
+                i++;
+                groups++;
+                if(groups > max) max = groups;
+            }
+            else{
+                j++;
+                groups--;
+            }
+        }
+        return max;
+        //still slow brrrr
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // Arrays.sort(intervals, (a, b) -> { return a[0] - b[0]; });
+        // for(int[] i : intervals){
+        //     if(pq.isEmpty() || pq.peek() < i[0]){
+        //         pq.poll();
+        //     }
+        //     pq.add(i[1]);
+        // }
+        // return pq.size();
 
         //sort them to decrease time complexity
         // Arrays.sort(intervals, (a, b) -> { return a[0] - b[0]; });
