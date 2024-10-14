22d94172294f4ac563b382f92c86fe760339d0a7
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-15 00:40:45 +0530

    Time: 17 ms (15.6%), Space: 48.9 MB (24.03%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -5,12 +5,23 @@ class Solution {
         for(int i : nums){
             mp.put(i, mp.getOrDefault(i, 0) + 1);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        //updating is hard in priorty Queue
-        //i can use sorted indices
-        mp.forEach((h ,v) -> pq.add(new int[] {h ,v}));
-        for(int i = 0; i < k; i++){
-            ans[i] = pq.poll()[0];
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        // //updating is hard in priorty Queue
+        // //i can use sorted indices
+        // mp.forEach((h ,v) -> pq.add(new int[] {h ,v}));
+        // for(int i = 0; i < k; i++){
+        //     ans[i] = pq.poll()[0];
+        // }
+        int[][] count = new int[nums.length][2];
+        Iterator it = mp.entrySet().iterator();
+        int i = 0;
+        while(it.hasNext()){
+            Map.Entry e = (Map.Entry) it.next();
+            count[i++] = new int[] {(int)e.getKey(), (int)e.getValue()};
+        }
+        Arrays.sort(count, (a, b) -> b[1] - a[1]);
+        for(i = 0; i < k; i++){
+            ans[i] = count[i][0];
         }
         return ans;
     }
