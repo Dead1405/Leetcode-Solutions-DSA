3c1a054fed041ae5a09a30b1fc5185e1284e1acf
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-15 23:31:28 +0530

    Time: 7 ms (100%), Space: 45.1 MB (86.38%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,13 +1,10 @@
 class Solution {
     public long minimumSteps(String s) {
         long count = 0;
-        for(char c: s.toCharArray()){
-            if(c == '0') count++;
-        }
         long ans = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(i < count && s.charAt(i) == '1') ans += count - i;
-            else if (i >= count && s.charAt(i) == '0') ans += i - count;
+        for(char c: s.toCharArray()){
+            if(c == '1') count++;
+            else ans += count;
         }
         return ans;
     }
