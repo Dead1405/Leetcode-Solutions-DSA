e9d9a6151660bad9a706cdfdde7dba330932d1ae
Dead1405 <159671606+Dead1405@users.noreply.github.com>
2024-10-25 12:19:56 +0530

    Time: 0 ms (100%), Space: 42.5 MB (5.3%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m  + n - 1;
+        while(m >= 1 && n >= 1){
+            if(nums1[m - 1] > nums2[n - 1]){
+                nums1[k--] = nums1[--m];
+            }
+            else nums1[k--] = nums2[--n];
+        }
+        while(m >= 1) nums1[k--] = nums1[--m];
+        while(n >= 1) nums1[k--] = nums2[--n];
+    }
+}
